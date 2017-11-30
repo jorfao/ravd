d5261cef31a4438517debb11bf5b54d747e53520
jorfao <joao.p.orfao@gmail.com>
2017-11-30 10:55:29 +0000

    Update ravd.bat
    Handle parameters. Check if avd exists and sdk location is right. Fix running when multiple devices are connected. Add command feedback.

--- ravd.bat
+++ ravd.bat
@@ -3,19 +3,35 @@
 SET spath=%~dp0
 SET sdk=%1
 SET avd=%2
+SET port=9898
+SET emulatorname=emulator-%port%
+
+IF /I "%1"=="" echo Error: SDK not passed. Usage: ravd [SDK] [AVD] &&EXIT /B
+IF /I "%2"=="" echo Error: AVD not passed. Usage: ravd [SDK] [AVD] &&EXIT /B
 
 cd %sdk%
+.\platform-tools\adb version >nul 2>&1||echo Error: SDK not found &&EXIT /B
+.\tools\emulator -list-avds >nul 2>&1|| findstr /I %2
 
-start "" /D ".\tools\" emulator -avd %avd% -partition-size 2047 -no-snapshot-load -verbose -writable-system
-
-echo wait for avd to boot
-.\platform-tools\adb wait-for-device
-
-.\platform-tools\adb root
-.\platform-tools\adb remount
-.\platform-tools\adb push %spath%su.pie /system/bin/su
-.\platform-tools\adb shell chmod 06755 /system/bin/su
-.\platform-tools\adb shell su --install
-.\platform-tools\adb shell "su --daemon&"
-.\platform-tools\adb shell setenforce 0
+if %errorlevel% == 0 (
+    start "" /D ".\tools\" emulator -avd %avd% -no-snapshot-load -verbose -writable-system -port 9898
+    echo Wait for avd to boot...
+    .\platform-tools\adb -s %emulatorname% wait-for-device
+    echo Getting root access...
+    .\platform-tools\adb -s %emulatorname% root
+    echo Remount...
+    .\platform-tools\adb -s %emulatorname% remount
+    echo Pushing su.pie...
+    .\platform-tools\adb -s %emulatorname% push %spath%su.pie /system/bin/su
+    echo Changing su permissions...
+    .\platform-tools\adb -s %emulatorname% shell chmod 06755 /system/bin/su
+    echo Running daemon...
+    .\platform-tools\adb -s %emulatorname% shell su --install
+    .\platform-tools\adb -s %emulatorname% shell "su --daemon&"
+    .\platform-tools\adb -s %emulatorname% shell setenforce 0
+    echo Success
+    cd %spath%
+) else (
+    echo Error: Emulator %2 not found &&EXIT /B
+)
 
